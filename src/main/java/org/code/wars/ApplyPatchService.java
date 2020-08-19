866cb40c3615c87cb1ed6e5c8b7eb6c8de48d91c
Artur Kovalchuk <artur.kovalchuk@worldapp.com>
2020-08-19 08:25:51 +0300

    Add .editorconfig

--- src/main/java/org/code/wars/ApplyPatchService.java
+++ src/main/java/org/code/wars/ApplyPatchService.java
@@ -15,58 +15,58 @@ import org.jetbrains.ide.RestService;
 
 public class ApplyPatchService extends RestService {
 
-    static {
-        System.setProperty("idea.trusted.chrome.extension.id", "inicfikfgahabbmboppkmgopiiapdnjn");
+  static {
+    System.setProperty("idea.trusted.chrome.extension.id", "inicfikfgahabbmboppkmgopiiapdnjn");
+  }
+
+  @Override
+  public boolean isSupported(@NotNull FullHttpRequest request) {
+    return true;
+  }
+
+  @NotNull
+  @Override
+  protected String getServiceName() {
+    return "keymapSwitcher";
+  }
+
+  @Override
+  protected boolean isMethodSupported(@NotNull HttpMethod httpMethod) {
+    return httpMethod == HttpMethod.POST;
+  }
+
+  @Nullable
+  @Override
+  public String execute(@NotNull QueryStringDecoder queryStringDecoder, @NotNull FullHttpRequest fullHttpRequest,
+                        @NotNull ChannelHandlerContext channelHandlerContext) {
+    // may be use ProjectManager
+    Project project = getLastFocusedOrOpenedProject();
+
+    if (project != null) {
+      ApplicationManager.getApplication().invokeLater(
+              () -> new ApplyPatchFromClipboardAction.MyApplyPatchFromClipboardDialog(project, "Index: src/Test.java\n" +
+                      "IDEA additional info:\n" +
+                      "Subsystem: com.intellij.openapi.diff.impl.patch.CharsetEP\n" +
+                      "<+>UTF-8\n" +
+                      "===================================================================\n" +
+                      "--- src/Test.java\t(date 1596695162128)\n" +
+                      "+++ src/Test.java\t(date 1596695162128)\n" +
+                      "@@ -0,0 +1,2 @@\n" +
+                      "+public class Test {\n" +
+                      "+}\n").show(),
+              ModalityState.any());
+
+      sendOk(fullHttpRequest, channelHandlerContext);
+      return null;
     }
 
-    @Override
-    public boolean isSupported(@NotNull FullHttpRequest request) {
-        return true;
-    }
+    return "No open project";
+  }
 
-    @NotNull
-    @Override
-    protected String getServiceName() {
-        return "keymapSwitcher";
-    }
-
-    @Override
-    protected boolean isMethodSupported(@NotNull HttpMethod httpMethod) {
-        return httpMethod == HttpMethod.POST;
-    }
-
-    @Nullable
-    @Override
-    public String execute(@NotNull QueryStringDecoder queryStringDecoder, @NotNull FullHttpRequest fullHttpRequest,
-                          @NotNull ChannelHandlerContext channelHandlerContext) {
-        // may be use ProjectManager
-        Project project = getLastFocusedOrOpenedProject();
-
-        if (project != null) {
-            ApplicationManager.getApplication().invokeLater(
-                    () -> new ApplyPatchFromClipboardAction.MyApplyPatchFromClipboardDialog(project, "Index: src/Test.java\n" +
-                            "IDEA additional info:\n" +
-                            "Subsystem: com.intellij.openapi.diff.impl.patch.CharsetEP\n" +
-                            "<+>UTF-8\n" +
-                            "===================================================================\n" +
-                            "--- src/Test.java\t(date 1596695162128)\n" +
-                            "+++ src/Test.java\t(date 1596695162128)\n" +
-                            "@@ -0,0 +1,2 @@\n" +
-                            "+public class Test {\n" +
-                            "+}\n").show(),
-                    ModalityState.any());
-
-            sendOk(fullHttpRequest, channelHandlerContext);
-            return null;
-        }
-
-        return "No open project";
-    }
-
-    @Override
-    protected boolean isHostTrusted(@NotNull FullHttpRequest request,
-                                    @NotNull QueryStringDecoder urlDecoder) {
-        return true;
-    }
+  @Override
+  protected boolean isHostTrusted(@NotNull FullHttpRequest request,
+                                  @NotNull QueryStringDecoder urlDecoder) {
+    return true;
+  }
 
 }
